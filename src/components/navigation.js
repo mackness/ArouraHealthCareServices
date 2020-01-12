242fd8cb0d48420f45ca639fe073cb4aed994d4f
Mack Solomon <macksol@gmail.com>
2020-01-11 18:10:00 -0800

    remove black header background color

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -7,7 +7,7 @@ function isActive(path) {
 
 export default function Navigation() {
   return (
-    <header className="bg-black-90 w-100 pv2 pv4-ns ph4-m ph5-l">
+    <header className="w-100 pv2 pv4-ns ph4-m ph5-l">
       <nav className="f6 fw6 ttu tracked mw8 center flex justify-center">
         <a
           className={`${isActive("/") ? "active" : ""} link dim dib mr3`}
