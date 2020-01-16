40f752f36ed6be231f66ee108879236c65de8b33
Mack Solomon <macksol@gmail.com>
2020-01-15 22:37:45 -0800

    use gatsby Link component

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,36 +1,6 @@
-require("dotenv").config()
-const fetch = require("node-fetch")
-
-exports.handler = async event => {
-  console.log(event)
-  //   console.log(`Recieved a submission: ${email}`)
-  //   var transporter = nodemailer.createTransport({
-  //     service: "gmail",
-  //     auth: {
-  //       user: "youremail@gmail.com",
-  //       pass: "yourpassword",
-  //     },
-  //   })
-
-  //   var mailOptions = {
-  //     from: "youremail@gmail.com",
-  //     to: "myfriend@yahoo.com",
-  //     subject: "Sending Email using Node.js",
-  //     text: "That was easy!",
-  //   }
-
-  //   function sendEmailAsync() {
-  //     return new Promise((resolve, reject) => {
-  //       transporter.sendMail(mailOptions, function(error, info) {
-  //         if (error) {
-  //           console.log(error)
-  //         } else {
-  //           console.log("Email sent: " + info.response)
-  //         }
-  //       })
-  //     })
-  //   }
-
-  //   return sendEmailAsync()
-  return Promise.resolve()
+exports.handler = function(event, context, callback) {
+  callback(null, {
+    statusCode: 200,
+    body: "Hello, World",
+  })
 }
