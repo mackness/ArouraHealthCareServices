741b1396196c10e1c88e3ec308184e98d6ca396b
Mack Solomon <macksol@gmail.com>
2020-02-01 15:55:31 -0800

    log

--- functions/handlers/newsletter-signup-form.js
+++ functions/handlers/newsletter-signup-form.js
@@ -4,21 +4,21 @@ const doc = new GoogleSpreadsheet(
   "1LbMQgmfrVYy9uGBB32q2KfELCBD-4z7kmhsznQlAYQw"
 )
 
-const credentials = {
-  type: process.env.GOOGLE_SERVICE_ACCOUNT_TYPE,
-  project_id: process.env.GOOGLE_SERVICE_ACCOUNT_PROJECT_ID,
-  private_key_id: process.env.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY_ID,
-  private_key: process.env.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY,
-  client_email: process.env.GOOGLE_SERVICE_ACCOUNT_CLIENT_EMAIL,
-  client_id: process.env.GOOGLE_SERVICE_ACCOUNT_CLIENT_ID,
-  auth_uri: process.env.GOOGLE_SERVICE_ACCOUNT_AUTH_URI,
-  token_uri: process.env.GOOGLE_SERVICE_ACCOUNT_TOKEN_URI,
-  auth_provider_x509_cert_url:
-    process.env.GOOGLE_SERVICE_ACCOUNT_AUTH_PROVIDER_CERT_URL,
-  client_x509_cert_url: process.env.GOOGLE_SERVICE_ACCOUNT_CLIENT_CERT_URL,
-}
-
 module.exports = function(body, callback) {
+  const credentials = {
+    type: process.env.GOOGLE_SERVICE_ACCOUNT_TYPE,
+    project_id: process.env.GOOGLE_SERVICE_ACCOUNT_PROJECT_ID,
+    private_key_id: process.env.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY_ID,
+    private_key: process.env.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY,
+    client_email: process.env.GOOGLE_SERVICE_ACCOUNT_CLIENT_EMAIL,
+    client_id: process.env.GOOGLE_SERVICE_ACCOUNT_CLIENT_ID,
+    auth_uri: process.env.GOOGLE_SERVICE_ACCOUNT_AUTH_URI,
+    token_uri: process.env.GOOGLE_SERVICE_ACCOUNT_TOKEN_URI,
+    auth_provider_x509_cert_url:
+      process.env.GOOGLE_SERVICE_ACCOUNT_AUTH_PROVIDER_CERT_URL,
+    client_x509_cert_url: process.env.GOOGLE_SERVICE_ACCOUNT_CLIENT_CERT_URL,
+  }
+  console.log({ credentials })
   doc.useServiceAccountAuth(credentials, function(err) {
     if (err) {
       console.log(
