ba15287daeceaa8463d405bb7613c85e1af748a6
Mack Solomon <macksol@gmail.com>
2020-02-01 12:36:41 -0800

    improve submission handling

--- functions/handlers/homepage-contact-form.js
+++ functions/handlers/homepage-contact-form.js
@@ -4,14 +4,12 @@ const client = require("twilio")(
   process.env.TWILLIO_ACCOUNT_AUTH_TOKEN
 )
 
-const { formatBodyString, decodePayload } = require("../helpers")
+const { formatBodyString } = require("../helpers")
 
-exports.default = function(event, callback) {
+exports.default = function(body, callback) {
   client.messages
     .create({
-      body: formatBodyString(
-        _.get(decodePayload(event.body), "ordered_human_fields")
-      ),
+      body: formatBodyString(_.get(body, "payload.ordered_human_fields")),
       from: process.env.TWILLIO_ACCOUNT_PHONE_NUMNER,
       to: process.env.TARGET_CELL_PHONE_NUMBER,
     })
--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,5 +1,7 @@
 require("dotenv").config()
+const _ = require()
 const { homePageContactForm } = require("./handlers")
+const { decodePayload } = require("./helpers")
 
 const forms = {
   HOMEPAGE_CONTACT_FORM: "homepage-contact-from",
@@ -9,9 +11,15 @@ exports.handler = function(event, context, callback) {
   console.log({ event })
   console.log({ context })
 
-  switch (event.body.form_name) {
+  const body = decodePayload(event.body)
+
+  switch (_.get(body, "payload.form_name")) {
     case forms.HOMEPAGE_CONTACT_FORM:
-      homePageContactForm(event, callback)
-      return
+      homePageContactForm(body, callback)
+      break
+    default:
+      console.log(
+        `Error(submission-created): Invalid submission, no matching form hanlder`
+      )
   }
 }
