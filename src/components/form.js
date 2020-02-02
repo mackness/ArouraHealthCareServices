a4c7bd9f4b96c7c2186090bf771e5efbefcd1910
Mack Solomon <macksol@gmail.com>
2020-02-02 09:29:05 -0800

    new form encoder

--- src/components/form.js
+++ src/components/form.js
@@ -18,9 +18,16 @@ class Form extends React.Component {
   }
 
   encode(data) {
-    return Object.keys(data)
-      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-      .join("&")
+    // return Object.keys(data)
+    //   .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    //   .join("&")
+    const formData = new FormData()
+
+    for (const key of Object.keys(data)) {
+      formData.append(key, data[key])
+    }
+
+    return formData
   }
 
   resetFormState = () => {
