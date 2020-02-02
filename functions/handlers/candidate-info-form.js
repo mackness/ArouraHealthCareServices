39b2c8c6eecd14ee5f35f836833d364f3f6d5c30
Mack Solomon <macksol@gmail.com>
2020-02-02 15:41:24 -0800

    horrible code

--- functions/handlers/candidate-info-form.js
+++ functions/handlers/candidate-info-form.js
@@ -2,8 +2,19 @@ const _ = require("lodash")
 const { sendTextMessage } = require("../actions")
 
 module.exports = function(body, callback) {
-  _.get(body, "payload.ordered_human_fields").forEach(field =>
-    console.log(field)
+  // _.get(body, "payload.ordered_human_fields").forEach(field =>
+  //   console.log(field)
+  // )
+
+  if (
+    _.get(
+      _.find(
+        _.get(body, "payload.ordered_human_fields"),
+        field => field.name === "newsletterCheckbox"
+      ),
+      "value",
+      "false"
+    ) === "true"
   )
-  sendTextMessage(_.get(body, "payload.ordered_human_fields"), callback)
+    sendTextMessage(_.get(body, "payload.ordered_human_fields"), callback)
 }
