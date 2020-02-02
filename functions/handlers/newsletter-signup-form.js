dfb3d2e69801dacc0d394b0f778076537c2c8c14
Mack Solomon <macksol@gmail.com>
2020-02-01 16:06:18 -0800

    format key

--- functions/handlers/newsletter-signup-form.js
+++ functions/handlers/newsletter-signup-form.js
@@ -9,7 +9,9 @@ module.exports = function(body, callback) {
     type: process.env.GOOGLE_SERVICE_ACCOUNT_TYPE,
     project_id: process.env.GOOGLE_SERVICE_ACCOUNT_PROJECT_ID,
     private_key_id: process.env.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY_ID,
-    private_key: process.env.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY,
+    private_key: process.env.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY.split(
+      "\\n"
+    ).join("\n"),
     client_email: process.env.GOOGLE_SERVICE_ACCOUNT_CLIENT_EMAIL,
     client_id: process.env.GOOGLE_SERVICE_ACCOUNT_CLIENT_ID,
     auth_uri: process.env.GOOGLE_SERVICE_ACCOUNT_AUTH_URI,
@@ -18,7 +20,6 @@ module.exports = function(body, callback) {
       process.env.GOOGLE_SERVICE_ACCOUNT_AUTH_PROVIDER_CERT_URL,
     client_x509_cert_url: process.env.GOOGLE_SERVICE_ACCOUNT_CLIENT_CERT_URL,
   }
-  console.log({ credentials })
   doc.useServiceAccountAuth(credentials, function(err) {
     if (err) {
       console.log(
