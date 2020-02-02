9b1293eeda1a7b584f421fe4486d23d56a23592f
Mack Solomon <macksol@gmail.com>
2020-02-02 11:21:14 -0800

    log candidate form body

--- functions/handlers/candidate-info-form.js
+++ functions/handlers/candidate-info-form.js
@@ -2,5 +2,6 @@ const _ = require("lodash")
 const { sendTextMessage } = require("../actions")
 
 module.exports = function(body, callback) {
+  console.log({ body })
   sendTextMessage(_.get(body, "payload.ordered_human_fields"), callback)
 }
